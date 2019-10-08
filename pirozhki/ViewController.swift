fe08e6a54e7607a25566d920628d4d0be4f54a42
xt4008 <56275770+xt4008@users.noreply.github.com>
2019-10-08 12:38:35 +0300

    добавил анимацию

--- pirozhki/ViewController.swift
+++ pirozhki/ViewController.swift
@@ -9,18 +9,36 @@
 import UIKit
  
 class ViewController: UIViewController {
+    @IBAction func moveButtonPressed(_ sender: Any) {
+        let newConstraint = NSLayoutConstraint(item: redView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: self.view.frame.width/2)
+        
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: .curveEaseOut , animations: {
+                self.view.removeConstraint(self.leadingConstraint)
+                self.view.addConstraint(newConstraint)
+                self.view.layoutIfNeeded()
+        }, completion: nil)
+        leadingConstraint = newConstraint
+
+    }
+    
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
     
     var clickCount=0
  
     
     @IBOutlet weak var labelName: UILabel!
     
-   
+    
     @IBAction func buttonClick(_ sender: UIButton) {
         
+
         clickCount+=1
-        labelName.text="Ты нажал кнопку \(clickCount) раз"
+        labelName.text="Ты съел \(clickCount) пирожков"
     }
     
  
+
 }
