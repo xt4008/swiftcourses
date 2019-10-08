59abbb7d454b2eab1719208b7b590635e22776a6
EvgeniyEs <rdbm@yandex.ru>
2019-10-08 21:33:30 +0300

    поправил код

--- pirozhki/ViewController.swift
+++ pirozhki/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
  
 class ViewController: UIViewController {
     @IBAction func moveButtonPressed(_ sender: Any) {
-        let newConstraint = NSLayoutConstraint(item: redView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: self.view.frame.width/2)
+       leadingConstraint.constant = 300
         
         UIView.animate(withDuration: 2.0, delay: 0.0, options: .curveEaseOut , animations: {
-                self.view.removeConstraint(self.leadingConstraint)
-                self.view.addConstraint(newConstraint)
+              
+                
                 self.view.layoutIfNeeded()
         }, completion: nil)
-        leadingConstraint = newConstraint
+        
 
     }
     
